b5020e02217b4837569ecff07e35efb04a0961fd
basavarajgudageri <basavaraj.gudageri@robosoftin.com>
2021-02-13 20:56:55 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Reducer } from './src/redux/reducer';
+import { AppNavigator } from './src/router';
+import thunk from 'redux-thunk';
+
+// eslint-disable-next-line no-undef
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// eslint-disable-next-line no-unused-vars
+const logger = (store) => (next) => (action) => {
+  const result = next(action);
+  return result;
+};
+
+const store = createStore(
+  Reducer.reducer,
+  composeEnhancers(applyMiddleware(logger, thunk)),
+);
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    </>
+  );
+};
+
+export default App;
---
+++ src/services/api-constants.js
@@ -0,0 +1,17 @@
+export const ApiPaths = {
+  BASE_URL: 'https://api.pexels.com/v1',
+  //   BASE_URL: '', for static API response
+  PHOTOS: '/search?query=people',
+  VIDEOS: '/videos/search?query=people',
+};
+
+export const AUTH = {
+  API_KEY: '563492ad6f917000010000014280e9d34f304dca8e48bd6e0fd08e00',
+};
+
+export const ApiMethods = {
+  GET: 'GET',
+  POST: 'POST',
+  DELETE: 'DELETE',
+  PATCH: 'PATCH',
+};
